f6c5059fe1bff21a66dbcd81dee4fe23d2ff0921
Kenji <fcb141021@yahoo.co.jp>
2023-10-11 23:39:08 +0900

    メッセージ機能を実装するためのコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :pets
   root 'pets#index'
   
+  resources :conversations do
+    resources :messages
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
