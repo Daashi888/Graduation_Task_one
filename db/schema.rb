fc525f84b6d4d942d44aa28a475172913b83250b
Kenji <fcb141021@yahoo.co.jp>
2023-10-11 11:30:14 +0900

    Favoriteモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_021527) do
+ActiveRecord::Schema.define(version: 2023_10_11_022515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_favorites_on_pet_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.text "image"
@@ -58,4 +67,6 @@ ActiveRecord::Schema.define(version: 2023_10_10_021527) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "favorites", "pets"
+  add_foreign_key "favorites", "users"
 end
