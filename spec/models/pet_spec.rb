4aa3cd8d2f611c1429d55ea92940220b49068187
Kenji <fcb141021@yahoo.co.jp>
2023-10-20 10:27:21 +0900

    モデルスペックにバリデーションに対するテストの内容を記述

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
-RSpec.describe Pet, type: :model do
+RSpec.describe 'ペットモデル機能', type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context '新規登録時に画像が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に本文が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に動物（犬、猫）の選択が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に年齢が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に性別が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に現在地が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に都道府県が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に去勢の有無が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '', vaccinated: '失敗テスト')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時にワクチン接種済みの有無が空の場合' do
+      it 'バリデーションにひっかる' do
+        pet = Pet.new(image: '失敗テスト', content: '失敗テスト', select_animal: '失敗テスト', age: '失敗テスト', gender: '失敗テスト', current_location: '失敗テスト', prefecture: '失敗テスト', castrated: '失敗テスト', vaccinated: '')
+        expect(pet).not_to be_valid
+      end
+    end
+    context '新規登録時に必要な項目をすべて入力した場合' do
+      it 'バリデーションが通る' do
+        pet = Pet.new(image: '成功テスト', content: '成功テスト', select_animal: '成功テスト', age: '成功テスト', gender: '成功テスト', current_location: '成功テスト', prefecture: '成功テスト', castrated: '成功テスト', vaccinated: '成功テスト')
+        expect(pet).to be_valid
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'ユーザーモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context '新規登録時に姓が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に名が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時にニックネームが空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に郵便番号が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に都道府県が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に市区町村が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に番地が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '', gender: '失敗テスト', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に性別が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '', occupation: '失敗テスト', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に職業が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '', role: '失敗テスト')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に権限種別が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(last_name: '失敗テスト', first_name: '失敗テスト', nickname: '失敗テスト', postal_code: '失敗テスト', prefecture: '失敗テスト',city: '失敗テスト', ward_town: '失敗テスト', gender: '失敗テスト', occupation: '失敗テスト', role: '')
+        expect(user).not_to be_valid
+      end
+    end
+    context '新規登録時に必要な項目をすべて入力した場合' do
+      it 'バリデーションが通る' do
+        user = User.new(last_name: '成功テスト', first_name: '成功テスト', nickname: '成功テスト', postal_code: '成功テスト', prefecture: '成功テスト',city: '成功テスト', ward_town: '成功テスト', gender: '成功テスト', occupation: '成功テスト', role: '成功テスト')
+        expect(user).to be_valid
+      end
+    end
+  end
 end
